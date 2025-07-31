d34c5bb2fcb9d7f3b9afbe7b2f717a52b68db624
RanHC30 <chajudran@gmail.com>
2025-07-31 10:16:37 -0600

    Agregar archivos de proyecto.

---
+++ WebAprendiendoMVC/Controllers/EmpresaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAprendiendoMVC.Controllers
+{
+    public class EmpresaController : Controller
+    {
+   
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+
+        public ActionResult Servicios()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+
+    }//Fin public class
+}//Fin namespace
